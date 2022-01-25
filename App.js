c5378a8ec6dc0fa2d8266b643ec6db39af8ec1db
yfarhod <yfarhod75@gmail.com>
2022-01-25 01:12:04 +0100

    update

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Meals To Go App!</Text>
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ padding: 16, backgroundColor: 'green' }}>
+          <Text>
+            Search
+          </Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: 'blue' }}>
+          <Text>
+            List
+          </Text>
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
