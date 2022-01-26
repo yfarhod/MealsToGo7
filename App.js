717b8e3bb995caa7e65b7a4ab238b515bbc3fbc6
yfarhod <yfarhod75@gmail.com>
2022-01-26 15:22:17 +0100

    upd with restaurants

--- App.js
+++ App.js
@@ -1,36 +1,13 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
-import { Searchbar } from "react-native-paper";
+
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
-  console.log(StatusBar.currentHeight);
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar placeholder="Search" />
-        </View>
-        <View style={styles.list}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { StatusBar, StyleSheet, View, SafeAreaView } from "react-native";
+import { Searchbar } from "react-native-paper";
+
+import { RestaurantInfo } from "../components/restaurant-info.component";
+
+export const RestaurantsScreen = () => (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.search}>
+      <Searchbar placeholder="Search" />
+    </View>
+    <View style={styles.list}>
+      <RestaurantInfo />
+    </View>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
