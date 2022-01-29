eae96250b1f89f4a231b5b51e09ffe48f832ec8b
yfarhod <yfarhod75@gmail.com>
2022-01-29 21:49:07 +0100

    upd

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card } from "react-native-paper";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import styled from "styled-components/native";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star";
@@ -36,16 +36,31 @@ const Rating = styled(View)`
   padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
+const Section = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled(View)`
+  flex-direction: row;
+  margin-left: auto;
+`;
+
+const TempClosed = styled(Text)`
+  color: ${(props) => props.theme.colors.ui.error};
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
     ],
     address = "100 some street",
     isOpenNow = true,
     rating = 4,
+    isClosedTemporarily = true,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
@@ -60,12 +75,22 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       />
       <Info>
         <Title>{name}</Title>
-        <Rating>
-          {ratingArray.map(() => (
-            <SvgXml xml={star} width={20} height={20} />
-          ))}
-          {!!isOpenNow && <SvgXml xml={open} />}
-        </Rating>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <TempClosed variant="label">CLOSED TEMPORARILY</TempClosed>
+            )}
+            <View style={{ paddingLeft: 16 }} />
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <View style={{ paddingLeft: 16 }} />
+            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+          </SectionEnd>
+        </Section>
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
