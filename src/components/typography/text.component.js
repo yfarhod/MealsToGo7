3b691d206c5ea7a5bc0cf38349eda37869ab66b6
yfarhod <yfarhod75@gmail.com>
2022-02-02 11:48:17 +0100

    text component added

---
+++ src/components/typography/text.component.js
@@ -0,0 +1,33 @@
+import styled from "styled-components/native";
+
+const defaultTextStyles = (theme) => `
+  font-family: ${theme.fonts.body};
+  font-weight: ${theme.fontWeights.regular};
+  color: ${theme.colors.text.primary};
+  flex-wrap: wrap;
+  margin-top: 0px;
+  margin-bottom: 0px;
+`;
+
+const body = (theme) => `
+    font-size: ${theme.fontSizes.body};
+`;
+
+const hint = (theme) => `
+    font-size: ${theme.fontSizes.body};
+`;
+
+const error = (theme) => `
+    color: ${theme.colors.text.error};
+`;
+
+const caption = (theme) => `
+    font-size: ${theme.fontSizes.caption};
+    font-weight: ${theme.fontWeights.bold};
+`;
+
+const label = (theme) => `
+    font-family: ${theme.fonts.heading};
+    font-size: ${theme.fontSizes.body};
+    font-weight: ${theme.fontWeights.medium};
+`;
