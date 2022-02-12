0be85437f9ae24ebd8eab2c11efaaa5b8a0ffe6d
yfarhod <yfarhod75@gmail.com>
2022-02-12 21:05:20 +0100

    added camelize

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,5 @@
 import { mocks } from "./mock";
+import camelize from "camelize";
 
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
@@ -15,7 +16,7 @@ const restaurantTransform = (result) => {
 };
 restaurantsRequest()
   .then(restaurantTransform)
-  .then((transformedResult) => console.log(transformedResult))
+  .then((transformedResult) => console.log(camelize(transformedResult)))
   .catch((err) => {
     console.log(err);
   });
