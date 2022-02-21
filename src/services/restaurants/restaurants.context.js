7307213c8aeaff107e8d074d9911293415887bb8
yfarhod <yfarhod75@gmail.com>
2022-02-22 00:02:47 +0100

    upd

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,12 +1,35 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 import { restaurantsRequest, restaurantTransform } from "./restaurants.service";
 
 export const RestaurantContext = createContext();
 
 export const RestaurantContextProvider = ({ children }) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const retrieveRestaurants = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      restaurantsRequest()
+        .then(restaurantTransform)
+        .then((result) => {
+          setIsLoading(false);
+          console.log(result);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err);
+        });
+    }, 2000);
+  };
+
+  useEffect(() => {
+    retrieveRestaurants();
+  }, []);
   return (
-    <RestaurantContext.Provider value={{ restaurants: [1, 2, 3, 4, 5, 6, 7] }}>
+    <RestaurantContext.Provider value={{ restaurants: [1, 2, 3] }}>
       {children}
     </RestaurantContext.Provider>
   );
